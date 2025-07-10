eb640efa381fada3fd4eeb0de9eae3f53b8ef6fd
IzumiQuan <2180928212@qq.com>
2025-07-10 21:40:45 +0800

    Update SearchCondition.java

--- src/main/java/com/group/edu/common/SearchCondition.java
+++ src/main/java/com/group/edu/common/SearchCondition.java
@@ -2,9 +2,14 @@ package com.group.edu.common;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class SearchCondition<T> {
     private int currentPage = 1;
     private int pageSize = 10;
     private T example;
+    private List<String> list;
+    private List<Integer> range;
+    private String orderBy;
 }
